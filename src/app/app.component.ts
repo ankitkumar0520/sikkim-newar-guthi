c7d42be6229b94a901aa5a6bb5dd844d82911e22
Ankit Kumar <kumarankit0520@gmail.com>
2025-08-10 19:37:55 +0530

    added navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './pages/common/navbar/navbar.component';
+import { FooterComponent } from './pages/common/footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sikkim-newar-guthi';
--- src/app/pages/common/navbar/navbar.component.ts
+++ src/app/pages/common/navbar/navbar.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  LucideAngularModule,
+  Mail,
+  Phone,
+  MapPin,
+  Facebook,
+  Instagram,
+  Twitter,
+  Youtube,
+  User,
+  Linkedin,
+  Send,
+  Menu,
+  X,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  imports: [CommonModule, LucideAngularModule],
 })
 export class NavbarComponent implements OnInit {
+  icons = {
+    Mail,
+    Phone,
+    MapPin,
+    Facebook,
+    Instagram,
+    Twitter,
+    Youtube,
+    Linkedin,
+    User,
+    Send,
+    Menu,
+    X,
+  };
 
-  constructor() { }
+  tabs = [
+    { label: 'Home', href: '#' },
+    { label: 'About Us', href: '#' },
+    { label: 'Shop', href: '#' },
+    { label: 'Pages', href: '#' },
+    { label: 'Blog', href: '#' },
+    { label: 'Contact', href: '#' },
+  ];
 
-  ngOnInit() {
+  isSidebarOpen = false;
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 
+  closeSidebar(): void {
+    this.isSidebarOpen = false;
+  }
+
+  ngOnInit() {}
 }

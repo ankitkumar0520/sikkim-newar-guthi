b129e327e99c873b709a3004f92569ace2b1c556
Ankit Kumar <kumarankit0520@gmail.com>
2025-08-20 09:20:50 +0530

    added netlify support

--- src/app/pages/common/footer/footer.component.ts
+++ src/app/pages/common/footer/footer.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule, MapPin, Phone, Mail } from 'lucide-angular';
+
+interface FooterLink {
+  label: string;
+  href: string;
+}
+
+interface ContactInfo {
+  type: string;
+  value: string;
+  icon: any;
+}
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  icons = {
+    MapPin,
+    Phone,
+    Mail,
+  };
 
-  constructor() { }
+  quickLinks: FooterLink[] = [
+    { label: 'About Us', href: '#' },
+    { label: 'Our Mission', href: '#' },
+    { label: 'Events', href: '#' },
+    { label: 'Gallery', href: '#' },
+    { label: 'Contact', href: '#' },
+  ];
 
-  ngOnInit() {
-  }
+  moreLinks: FooterLink[] = [
+    { label: 'Membership', href: '#' },
+    { label: 'Resources', href: '#' },
+    { label: 'News', href: '#' },
+    { label: 'Support', href: '#' },
+    { label: 'FAQ', href: '#' },
+  ];
 
+  contactInfo: ContactInfo[] = [
+    {
+      type: 'Address',
+      value: 'Gangtok, Sikkim, India , Random for sampple',
+      icon: MapPin,
+    },
+    { type: 'Mobile', value: '+91 3592 000 000', icon: Phone },
+    { type: 'Email', value: 'contact@sikkimnewarguthi.org', icon: Mail },
+  ];
 }
